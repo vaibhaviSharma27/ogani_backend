aa58fe00fe4bac72407f8b98f1ac3405462f10e7
Vaibhavi <vaibhavisharma.sv2527@gmail.com>
2026-07-02 17:30:03 +0530

    as

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import bcrypt from "bcrypt";
-import jwt from "jsonWebToken";
+import jwt from "jsonwebtoken";
 import "dotenv/config";
 
 import cookieParser from "cookie-parser";
