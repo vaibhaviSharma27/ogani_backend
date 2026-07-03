6ec8a843e88a4496704e117ff53cc665f926f803
Vaibhavi <vaibhavisharma.sv2527@gmail.com>
2026-07-03 15:12:00 +0530

    changed cors

--- index.js
+++ index.js
@@ -23,13 +23,11 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://vaibhavigroceries.vercel.app",
     credentials: true
 }));
 
 app.use("/image", express.static(path.join(process.cwd(),"uploads")));
-
-
 
 
 // Establish db connection
